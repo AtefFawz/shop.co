95b69ab92ed3c988c365f9b9d0c79d6d8928dada
Atef Fawzy <ateffawzy897@gmail.com>
2026-08-18 15:46:50 +0300

    Update server port configuration to 4000 and adjust listen method for broader accessibility

--- shop_app.js
+++ shop_app.js
@@ -120,10 +120,10 @@ socketConnection(io);
 /* Set Socket.IO Instance */
 setIO(io);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 4000;
 
-server.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
 
 module.exports = app;
